e4590aa13c7615280edc035ca669398c85fac4c0
Shreyash Chandra <puttashreyash@gmail.com>
2022-09-28 15:26:46 +0530

    Update lines.py

--- assignment4/lines.py
+++ assignment4/lines.py
@@ -13,22 +13,19 @@ import shlex
 A = np.array([1,0]) 
 B = np.array([2,3]) 
 
-#The direction vector of the line joining two points A(1,0), B(2,3) is given by
-#M=A−B
+#The direction vector of the line joining two points A(1,0) & B(2,3) is given by "M=A−B"
 M = np.array(A-B)
 
 # position/directional vector of the A&B line segment used as the normal vector of its perpendicular line
-print ("Equation of perpendicual line is:")
+print ("Equation of perpendicular line is:")
 print(M,"X =", "C")
-f  = int(input("enter your required value in 1:n ratio n :  "))  
+n  = int(input("enter your required n for 1:n ratio (n>=0) :  "))  
 
-#The equation of the line perpendicular to line and passing through the point P is given by
-#m⊤ (x − P) = 0
-u= (2+f)/(1+f)
-e= (3)/(1+f)
-x = np.array([u,e])
-m = M@x       #normal vector m 
-print ("Equation of the perpendicual line through point is")
-print (m,"X +",k,"= 0")
-#The point P that divides the line segment AB in the ratio 1 : k is given by
-#P= (B + kA)/(1+k)
+#The equation of the line perpendicular to #n⊤ (x) = C and passing through the point P is given by "m⊤ (x − P) = 0"
+#The point P that divides the line segment AB in the ratio 1 : n is given by#P= (B + nA)/(1+n)
+u= (2+n)/(1+n)   
+e= (3)/(1+n)   
+p = np.array([u,e]) #abtained point P by section formula in 1:n ratio 
+C = M@p       #Constant C 
+print ("Equation of the perpendicual line through 1 :",n,"rato is:")
+print (M,"X =",C)
