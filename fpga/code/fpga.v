b997d6f6297ea29c5f3645b49994a255b14805d1
Shreyash Chandra <puttashreyash@gmail.com>
2022-11-01 15:58:23 +0530

    Update fpga.v

--- fpga/code/fpga.v
+++ fpga/code/fpga.v
@@ -24,3 +24,8 @@ B= Y||Z;
 
 end
 endmodule
+
+
+#!/bin/bash
+
+ql_symbiflow -compile -src /sdcard/Download/iith/iith/vaman/Assignment-10/codes -d ql-eos-s3 -P PU64 -v Assignment10.v -t helloworldfpga -p Mypins.pcf -dump binary
