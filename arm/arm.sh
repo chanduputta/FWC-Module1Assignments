66febb4d593e4386865d62250533fd9cad7419b8
Shreyash Chandra <puttashreyash@gmail.com>
2022-11-01 14:21:14 +0530

    Delete arm.sh

--- arm/arm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#this instruction is to be run on pi for flashing into vaman
-
-sudo python3 /home/shreyash/armencoder/TinyFPGA-Programmer-Application/tinyfpga-programmer-gui.py --port /dev/ttyACM0  --appfpga top.bin --m4app blink.bin --mode m4-fpga --reset
