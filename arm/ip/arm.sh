517168e6ea039e5f275d49e5d57e19765faf6c20
chanduputta <puttashreyash@gmail.com>
2022-11-04 15:07:13 +0530

    ip

---
+++ arm/ip/arm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#this instruction is to be run on pi for flashing into vaman
+
+sudo python3 /home/shreyash/ip/TinyFPGA-Programmer-Application/tinyfpga-programmer-gui.py --port /dev/ttyACM0  --appfpga top.bin --m4app encoder.bin --mode m4-fpga --reset
